19e0c0b5aac9790d89e9acab50a08819733cebf8
cazdemun <cplu1509@gmail.com>
2019-10-01 21:30:05 -0500

    Commit first unity test||

---
+++ Assets/GlobalRetriever.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalRetriever : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hola");
+        Debug.Log(Globals.MiVar);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/StateMachineManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class StateMachineManager : MonoBehaviour
+{
+    public GameObject inputField;
+    public string asdf;	
+    public TMP_Text m_TextComponent;
+
+    public void StartGrazScene()
+    {
+        // load a new scene
+        SceneManager.LoadScene(1);
+	asdf = m_TextComponent.GetComponent<TMP_Text>().text;
+	Debug.Log(asdf);
+    }
+
+    public void OnChangeLogger() 
+    {
+	Debug.Log("it shoudl work but it doesnt");
+	//asdf = inputField.GetComponent<Text>().text;
+	asdf = m_TextComponent.GetComponent<TMP_Text>().text;
+	Debug.Log(asdf);
+    }
+
+    public void Extra(TMP_Text wide) 
+    {
+	Debug.Log("it shoudl work but it doesnt");
+	asdf = wide.GetComponent<TMP_Text>().text;
+	Debug.Log(asdf);
+    }
+}
